0c370e1f403ed2dc769a418bf75ec84258eb676d
m01rad <murad_rehimov_01@mail.ru>
2022-05-02 08:17:51 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    static Multidimensional multidimensional;
+
+    public static void createObj(int a, int b){
+        if(a == b) multidimensional = new Multidimensional(a,b);
+
+        else {
+            System.out.println("Try again");
+            System.out.print("columnSize: ");
+            int columnCount = scanner.nextInt();
+            System.out.print("rowSize: ");
+            int rowCount = scanner.nextInt();
+            createObj(columnCount, rowCount);
+        }
+    }
+
+    public static void main(String[] ar){
+        Scanner scanner = new Scanner(System.in);
+      Object o = new Object();
+
+        System.out.print("columnSize: ");
+       int columnCount = scanner.nextInt();
+        System.out.print("rowSize: ");
+        int rowCount = scanner.nextInt();
+        createObj(columnCount ,rowCount);
+
+        multidimensional.enterValues();
+
+       System.out.println(multidimensional.getFirstDiogonalElements());
+       System.out.println(multidimensional.getSecondDiogonalElements());
+        System.out.println(multidimensional.toMatrixForm());
+    }
+}
---
+++ src/Multidimensional.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Multidimensional {
+    public int columnSize ;
+    public int rowSize;
+    public int[][] array;
+    static Multidimensional n = null;
+    private Scanner scanner = new Scanner(System.in);
+    public String fDE = "";
+
+    public String sDE = "";
+    public String string = "";
+
+    Multidimensional(int columnSize , int rowSize ){
+
+        this.array = new int[columnSize][rowSize];
+    }
+
+    Multidimensional(){
+
+    }
+    public void enterValues(){
+        for(int i = 0 ; i<this.array.length;i++){
+            for (int j= 0 ; j<this.array[i].length ;j++){
+
+                this.array[i][j] = scanner.nextInt();
+                fDE= i == j  ? fDE + this.array[i][j] +" " :fDE;
+                sDE =i == (this.array.length-1) -j ? sDE+ this.array[i][j] +" " : sDE;
+            }
+        }
+
+    }
+
+    public String getFirstDiogonalElements(){
+
+           return "firstDigonalElem: " +fDE;
+    }
+    public String getSecondDiogonalElements(){
+
+        return  "seconDigonalElements: "+ sDE;
+    }
+     public String toMatrixForm(){
+         for (int[] arrays: this.array
+              ) {
+             for (int elem: arrays
+                  ) {
+                 string += elem + " ";
+             }
+             string +='\n';
+         }
+         return string;
+     }
+
+    @Override
+
+    public String toString(){
+        return ("Multidimensional{" + '\n'+
+                    "   rowSize: " + rowSize + '\n'+
+                    "   columnSize: " +columnSize + '\n'+
+                 "}" );
+    }
+
+}
